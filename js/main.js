0e720d6d9dc1a3b4398e8af7542facb162d21cb2
leannmeier <leandromeier56@gmail.com>
2024-04-28 23:06:09 -0300

    Variables en JS

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ alert("Soy JavaScript!");
 acceso rapido a varios comentarios */
 
 //acceso rapido a una linea de comentarios
+
+//Variables
+let mensaje = "Hola Mundo";
+alert("usando variables de tipo 'let' "+ mensaje);
+
+const pi = Math.PI;
+alert("Usando variables de tipo 'const' "+pi);
+
+var numero = 13;
+alert("Usando variables de tipo 'var' "+numero);
