6dcfecaa562304861e4285bb578116d36815a5e2
leannmeier <leandromeier56@gmail.com>
2024-05-02 21:14:31 -0300

    Tipos de conversiones

---
+++ 01-El Lenguaje JavaScript/02-Fundamentos de JavaScript/js2.7/main.js
@@ -0,0 +1,8 @@
+//Conversiones de tipos
+
+//Conversion de cadenas
+let value = true;
+alert(typeof value); //boolean
+
+value = String(value); //ahora el contenido del String es "true"
+alert(typeof value); //String
