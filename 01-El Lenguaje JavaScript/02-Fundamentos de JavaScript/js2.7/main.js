e0a4ac6981cb9e964658806cf725fc778b507254
leannmeier <leandromeier56@gmail.com>
2024-05-02 21:18:02 -0300

    Cambio el nombre del script

--- 01-El Lenguaje JavaScript/02-Fundamentos de JavaScript/js2.7/main.js
+++
@@ -1,8 +0,0 @@
-//Conversiones de tipos
-
-//Conversion de cadenas
-let value = true;
-alert(typeof value); //boolean
-
-value = String(value); //ahora el contenido del String es "true"
-alert(typeof value); //String
