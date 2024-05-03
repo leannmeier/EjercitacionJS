5082949696a2ed21c5e4456198c8b18af8b83a82
leannmeier <leandromeier56@gmail.com>
2024-05-02 21:56:05 -0300

    Operadores basicos, matematicos

---
+++ 01-El Lenguaje JavaScript/02-Fundamentos de JavaScript/js2.8/main2-8.js
@@ -0,0 +1,28 @@
+//Operadores basicos, matematicos
+
+//Operador unitario
+let number = 1;
+number = -number; //oeprador de negacion
+alert(number); //imprime -1
+
+//Operador binario
+let x = 1, y = 3;
+alert(y-x); // 2
+// Un operador es binario si tiene dos operandos
+
+/*Operadores matematicos
+- (+) suma
+- (-) resta
+- (*) producto
+- (/) cociente
+- (%) resto de una division -> a%b = resto de a/b
+- (**) exponenciacion -> a ** b = a ^ b | para una raiz -> alert( 4 ** (1/2) ); //Imprime 2
+*/
+
+//Concatenacion de cadenas
+let str = "my" + "string";
+alert(str); // Imrpime "mystring";
+
+alert("1"+2);//"12" como cadena
+alert(2+"1"+2);//"212" como cadena
+alert(2+2+"1"+2);//"412" como cadena
